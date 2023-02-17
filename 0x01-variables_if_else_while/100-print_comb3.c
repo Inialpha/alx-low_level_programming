24cd0b1049a2615432fd9e9beda1140379291511
Initech <inimfonebong001@gmail.com>
2023-02-17 12:09:38 -0800

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return 0 (Success)
+ */
+
+int main()
+{
+	int num1;
+	for (num1 = 0; num1 <= 99; n++)
+	{
+		putchar('0' + num1 / 10);
+		putchar('0' + num1 % 10);
+		putchar(,);
+		putchar(' ');
+	}
+	return (0);
+}
