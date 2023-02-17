9bcbab1edc2682e078293ebaf4d0d97a0bee801f
Initech <inimfonebong001@gmail.com>
2023-02-17 14:34:52 -0800

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,22 @@
 /*
  * main - Entry point
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	int i;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
