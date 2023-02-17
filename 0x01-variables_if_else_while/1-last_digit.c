3343196c85ddf7887993d3ff5cc6ede2c9244f57
Initech <inimfonebong001@gmail.com>
2023-02-17 05:49:59 -0800

    c progamms

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int lastdigit
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n % 10;
+
+	if (lastdigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lastdigit);
+	}
+
+	else if (lastdigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+	}
+
+	else if (lastdigit < 6 && != 0)
+	{
+		printf("Last digit of %d is %d and \nis less than 6 and not 0\n", n, lastdigit);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	char capsletter = 'A';
+
+	while (letter <= z)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	while (capsletter = <= Z)
+	{
+		putchar(capsletter);
+		capsletter++;
+	}
+
+	putchar('\n');
+}
