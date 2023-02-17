923aed556945e67f8fa18d5a1c4739354782edfc
Initech <inimfonebong001@gmail.com>
2023-02-17 08:27:41 -0800

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char i = '0'
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= a)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
