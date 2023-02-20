678734d3a9b813aeb912c820597192efe2fb8167
Initech <inimfonebong001@gmail.com>
2023-02-20 11:58:49 -0800

    end

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: 0 (Success)
- */
 
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int num1;
-	int num2;
+	int digit1, digit2;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (num2 = num1 + 1; num < 10; num2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar('0' + (num1 % 10));
-			putchar('0' + (num1 % 10));
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-			if (if num1 == 8 && num2 == 9)
-			continue;
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
