ab3452546efd4251d16bc1b3f550f896ad5f7c94
Initech <inimfonebong001@gmail.com>
2023-02-20 11:17:49 -0800

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,10 +3,33 @@
 /**
  *main - Entry point
  *
- * Return 0 (success)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-        return (0);
+	int i, j;
+
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
