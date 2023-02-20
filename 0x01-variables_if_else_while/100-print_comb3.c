0e6f22e722ad426b2815d215968455de05e4d572
Initech <inimfonebong001@gmail.com>
2023-02-20 11:47:06 -0800

    task 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,21 @@
 int main(void)
 {
 	int num1;
+	int num2;
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		putchar('0' + num1 / 10);
-		putchar('0' + num1 % 10);
-		putchar(',');
-		putchar(' ');
+		for (num2 = num1 + 1; num < 10; num2++)
+		{
+			putchar('0' + (num1 % 10));
+			putchar('0' + (num1 % 10));
+
+			if (if num1 == 8 && num2 == 9)
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
