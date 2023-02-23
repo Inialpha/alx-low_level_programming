decf9799b2df276c6688617d1b2d2610b404e64a
Initech <inimfonebong001@gmail.com>
2023-02-23 09:37:36 -0800

    5&7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_diagonal - print a diagonal line
@@ -11,7 +11,9 @@ void print_diagonal(int n)
 	int j;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
