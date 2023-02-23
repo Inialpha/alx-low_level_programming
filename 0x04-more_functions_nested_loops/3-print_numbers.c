4955dc66e05b6ced33a9a1239f1862b5f01c1f91
Initech <inimfonebong001@gmail.com>
2023-02-23 08:43:31 -0800

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,11 @@
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(í);
 	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,15 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-	n = 
 
-	for
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
