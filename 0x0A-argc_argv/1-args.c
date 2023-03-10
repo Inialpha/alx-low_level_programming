95a1f1152545f2784c5b3ef760134f187d3b5eb6
Initech <inimfonebong001@gmail.com>
2023-03-10 09:49:40 -0800

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*main - counts it arguments
+ * @argc: arg count
+ * argv vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
