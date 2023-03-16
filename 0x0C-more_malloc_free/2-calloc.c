5ffde11e8161efa43cdc6d9cec6f0b2fef700829
Initech <inimfonebong001@gmail.com>
2023-03-16 12:33:21 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,9 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = malloc(nmemb * sizeof(size));
+	char *ptr;
+	unsigned int i;
+
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -16,9 +18,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 
 
+	ptr = malloc(nmemb * sizeof(size));
 	if (ptr == NULL)
 		return (NULL);
-
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0;
 	return (ptr);
 }
-
