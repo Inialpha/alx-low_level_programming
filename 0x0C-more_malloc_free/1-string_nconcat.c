0411a849066710ed2ae9ccd00e18d08712e53523
Initech <inimfonebong001@gmail.com>
2023-03-16 11:26:17 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,36 +11,50 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
-	int len1 = 0;
+	unsigned int len1 = 0;
 	int _strlen = 0;
 	char *string;
-	int len2 = 0;
+	unsigned int len2 = 0;
 
 	int c = 0;
 
-	while (s1[c] != '\0')
+	if (s1 == NULL)
+		len1 = 0;
+	else
 	{
-		c++;
-		len1++;
+		while (s1[c] != '\0')
+		{
+			c++;
+			len1++;
+		}
 	}
 
 	c = 0;
-	while (s2[c] != '\0')
+	if (s2 == NULL)
+		len2 = 0;
+	else
 	{
-		c++;
-		len2++;
+		while (s2[c] != '\0')
+		{
+			c++;
+			len2++;
+		}
 	}
 	_strlen = len1 + len2;
+	
+	if (len2 > n)
+		len2 = n;
 
-	string = malloc(_strlen * sizeof(char));
+
+	string = malloc((_strlen + 1) * sizeof(char));
 	if (string == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		*(string + i) = s1[i];
 
 
-	for (j = 0; s2[j] != '\0' && j <= n; j++)
+	for (j = 0; j < len2; j++)
 		*((string + i) + j) = s2[j];
 
 	*((string + i) + j) = '\0';
