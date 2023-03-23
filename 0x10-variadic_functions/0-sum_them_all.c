f9c7029c4b8e9b146b83e342e516e7b5ae806337
Initech <inimfonebong001@gmail.com>
2023-03-23 10:35:11 -0700

    Make correction on the initialization of va_start str

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	int digit = 0;
 	int sum = 0;
 
+	va_start(numbers, n);
 	if (n == 0)
 		return (0);
 
-	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
 		digit = va_arg(numbers, int);
