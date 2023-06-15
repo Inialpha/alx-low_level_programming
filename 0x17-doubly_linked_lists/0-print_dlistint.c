c69a1e5da1411bd2cdc592d5fb3a400108c3cc90
Initech <inimfonebong001@gmail.com>
2023-06-15 04:04:13 -0700

    update to meet betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,21 +2,21 @@
 
 
 /**
- * print_dlistint print a doubly LL
+ * print_dlistint - print a doubly LL
  * @h: pointer to the first node in the list
- *
+ * Return: number if elements printed
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	
+
 	return (count);
 }
