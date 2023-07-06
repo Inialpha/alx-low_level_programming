4c85bc79957061f9ede94b2f2a60a20cc4ee2634
Initech <inimfonebong001@gmail.com>
2023-07-06 13:11:26 -0700

    handle empty arrays

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *temp;
 	unsigned long int i = 0, flag = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 	printf("{");
 	for (; i < ht->size; i++)
