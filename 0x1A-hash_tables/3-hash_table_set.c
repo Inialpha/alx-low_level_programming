bd732266f1eb7de1b9a148ed2994630211f131ec
Initech <inimfonebong001@gmail.com>
2023-07-06 12:43:36 -0700

    handle empty key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,7 +45,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *temp;
 
-	if (key == NULL || ht->array == NULL || ht->size == 0 || value == NULL)
+	if (key == NULL || ht->array == NULL || ht->size == 0 ||
+			value == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
