49cc7ad48d13accbb7c30d10ea3c619609fe50d6
Initech <inimfonebong001@gmail.com>
2023-07-08 03:04:28 -0700

    handle segmrntation fault

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,20 +13,20 @@ hash_node_t *create_node(const char *key, const char *value)
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (NULL);
 	new_node->next = NULL;
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node);
-		return (0);
+		return (NULL);
 	}
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node->value);
 		free(new_node);
-		return (0);
+		return (NULL);
 	}
 	return (new_node);
 
@@ -45,8 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *temp;
 
-	fflush(stdout);
-	if (key == NULL || ht->array == NULL || ht->size == 0 ||
+	if (key == NULL || ht == NULL || ht->array == NULL || ht->size == 0 ||
 			value == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
