39a64caad4a65e0a70528183fe4253aa350e377d
Initech <inimfonebong001@gmail.com>
2023-07-08 02:48:02 -0700

    include temp->next

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,6 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *temp;
 
+	fflush(stdout);
 	if (key == NULL || ht->array == NULL || ht->size == 0 ||
 			value == NULL || strlen(key) == 0)
 		return (0);
@@ -60,14 +61,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp->value = strdup(value);
 			if (temp->value == NULL)
 				return (0);
+			fflush(stdout);
 			return (1);
 		}
+		temp = temp->next;
 	}
 	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+	fflush(stdout);
 
 	return (1);
 }
