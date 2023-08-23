7c5972626052f0d9acec23d922b7ac9d8b687a64
Initech <inimfonebong001@gmail.com>
2023-08-23 07:23:07 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""calaculates island parameter"""
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid:"""
+    i = 0
+    ln = len(grid)
+    para = 0
+    while i < ln:
+        row_len = len(grid[i])
+        j = 0
+        while j < row_len:
+            if grid[i][j] == 1:
+                para += 4
+                if i != 0:
+                    if grid[i - 1][j] == 1:
+                        para -= 1
+                    if grid[i + 1][j] == 1:
+                        para -= 1
+                    if grid[i][j - 1] == 1:
+                        para -= 1
+                    if grid[i][j + 1] == 1:
+                        para -= 1
+            j += 1
+        i += 1
+    return para
