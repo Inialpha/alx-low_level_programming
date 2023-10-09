5e2d00c194bac056a473ef0c34bb4a9c4d483db9
Initech <inimfonebong001@gmail.com>
2023-10-09 07:14:44 -0700

    Add everthing.in the repo

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - perform jump serach
+ * @array: array to be searched
+ * @size: array size
+ * @value: value to search for
+ * Return: index of value if found or -1
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int sq;
+	size_t i, j;
+
+	if (!array)
+		return (-1);
+
+	sq = sqrt(size);
+
+	for (i = 0; i < size; i += sq)
+	{
+		if (value <= array[i])
+			break;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	}
+	if (array[i] >= value)
+	{
+		printf("Value found between indexes [%ld] and [%ld]\n", i - sq, i);
+		for (j = i - sq; j <= i; j++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+			if (value == array[j])
+				return (j);
+		}
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i - sq, i);
+	for (j = i - sq; j < size; j++)
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+
+	return (-1);
+}
+
+
