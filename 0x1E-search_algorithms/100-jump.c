523e91027013487f41480f5960e82054df16dcd2
Initech <inimfonebong001@gmail.com>
2023-10-12 07:05:40 -0700

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,21 +15,21 @@ int jump_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
-
 	sq = sqrt(size);
 	for (i = 0; i < size; i += sq)
 	{
 		if (value <= array[i])
 			break;
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
 	}
 
 	j = i - sq;
 	printf("Value found between indexes [%ld] and [%ld]\n", j, i);
 	i = i < size ? i : size - 1;
-	for (; j <= i && array[j] <= value; j++)
+	for (; j <= i && array[j] <= value && j < size - 1; j++)
+	{
 		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+	}
 
 	return (array[j - 1] == value ? j : (size_t)-1);
 }
